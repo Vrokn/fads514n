e7b6bf85e6da46bea156efdf0c08ee3aa20be7f4
Mateo <mgomezossa@gmail.com>
2020-05-05 18:59:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       list: ['Javascript', 'CSS', 'HTLM', 'React']
     }
   }
-  render() {
+  render() { 
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
